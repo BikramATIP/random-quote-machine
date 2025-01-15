13b8e5883e719e3f6382d4aab197e5ba5f559556
Bikram Thapa <bikramt755@gmail>
2025-01-15 15:03:21 +1100

    style quotes and optimise tags

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,38 @@ import './App.css'
 
 
 function App() {
-  const [quote, setQuote] = useState({"author": "Abraham Lincoln", "content": "Character is like a tree and reputation like a shadow. The shadow is what we think of it; the tree is the real thing.", "tags": "famous-quotes"})
+  const [quote, setQuote] = useState("")
   
   const updateQuote = () => {
     const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
     setQuote(randomQuote)
   }
   
+  const formatTag = (tag) => {
+   return tag.replace(/-/g, " ").replace(/\b\w/g, l => l.toUpperCase())
+  }
+
+
   return (
-    <>
+    <div className="container">
       <h1>Random Quote Generator</h1>
       <div className="card">
         <button onClick={updateQuote}>
           Get Quote
         </button>
-
-      </div>
-      <p className="read-the-docs">  
+        <p className="quote">  
        {quote.content}
       </p>
-    </>
+
+      <ul className="category-container">
+      { quote.tags.map((tag, index) => {
+        return <div className="wrapper"><li key={index} className="category">{formatTag(tag)}</li></div>
+      })}
+       
+     
+      </ul>
+      </div>
+    </div>
   )
 }
 
